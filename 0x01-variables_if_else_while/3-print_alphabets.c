bb29a12a078744ce6bdcef829ec056ca907bda33
sirmodika@gmail.com <enikma-mod>
2022-04-08 16:31:50 -0700

    2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * 
+ * RETURN: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+char lower_case;
+char upper_case;
+
+for (upper_case = 'n'; upper_case <= 'Z'; uppere++)
+{
+putchar(upper_case);
+}
+
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{
+putchar(lower_case)
+}
+
+putchar('\n');
+
+return (0)
+
+}
