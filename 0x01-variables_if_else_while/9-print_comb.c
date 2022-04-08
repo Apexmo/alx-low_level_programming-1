4e67a753de0d8e6cb83e2039b3fce827eeb91765
sirmodika@gmail.com <enikma-mod>
2022-04-08 14:38:58 -0700

    1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main - Entry point
- * 
- *Return: Always 0 (Success)
+*
+ Return: Always 0 (Success)
  */
 int main(void)
 
@@ -16,7 +16,7 @@ putchar(number);
 
 if (number == 57)
 {
-break;
+reak;
 }
 
 putchar(',');
