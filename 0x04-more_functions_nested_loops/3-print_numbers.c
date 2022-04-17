c6bbeb02a30a0101e6773e2215537299db79c4bd
Enikma <80250498+enikma-mod@users.noreply.github.com>
2022-04-17 15:20:19 +0200

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,10 +10,9 @@ void print_numbers(void)
     int i;
   
     i = 0;
-    while (i + '0')
-    {
-      _putchar (i + '0');
-      i++;
-    }
-    _putchar ('\n');
+    
+    for (i = 0; i < 10; i++)
+        _putchar(i + '0');
+    
+        _putchar ('\n');
 }
