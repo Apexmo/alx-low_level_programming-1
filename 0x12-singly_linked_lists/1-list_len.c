18be4b0fb1a11e7006748391d25f07e3c5c59b0f
enikma-mod <sirmodika@gmmail.com>
2022-05-19 14:24:43 +0200

    Task 1 - fixing betty style white spaces

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * list_len - Show the number of elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of elements of a list
-  */
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
+ */
 size_t list_len(const list_t *h)
 {
-	size_t elements_increment = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
+		elements++;
 		h = h->next;
-		elements_increment++;
 	}
 
-	return (element_increment);
+	return (elements);
 }
