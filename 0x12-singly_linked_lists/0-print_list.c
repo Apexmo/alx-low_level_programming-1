c1e40097a78ddf929e77fefa660e864371a4bc2a
enikma-mod <sirmodika@gmmail.com>
2022-05-19 14:11:19 +0200

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_list - Prints all the elements of a list_t list.
@@ -11,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0
+	size_t nodes = 0;
 
 	while (h)
 	{
